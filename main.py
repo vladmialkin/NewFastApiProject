9ee28a8fa6e72ee298aae669adf1f24549536b42
mialkin <mialkin@bolid.ru>
2024-02-05 10:45:33 +0300

    feat: Creating a router for User; Creating the main router application; Adding user_router to the main router; Adding the create_user function.

--- main.py
+++ main.py
@@ -80,4 +80,36 @@ class UserCreate(BaseModel):
         return value
 
 
-app = FastAPI()
+#  Создание экземпляра приложения
+app = FastAPI(title="Мой проект")
+
+user_router = APIRouter()  # создание router для User
+
+
+async def _create_new_user(body: UserCreate):
+    async with async_session() as session:
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(
+                name=body.name,
+                surname=body.surname,
+                email=body.email,
+            )
+            return ShowUser(
+                user_id=user.user_id,
+                name=user.name,
+                surname=user.surname,
+                email=user.email,
+                is_active=user.is_active,
+            )
+
+
+@user_router.post('/', response_model=ShowUser)
+async def create_user(body: UserCreate) -> ShowUser:
+    return await _create_new_user(body)
+
+
+main_api_router = APIRouter()
+
+main_api_router.include_router(user_router, prefix='/user', tags=['user'])
+app.include_router(main_api_router)
