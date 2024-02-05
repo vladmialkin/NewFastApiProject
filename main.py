53eca852bf263181e3f20af965248a2a85f6da80
mialkin <mialkin@bolid.ru>
2024-02-05 09:48:47 +0300

    create connection database

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import uvicorn
+from fastapi.routing import APIRouter
+from sqlalchemy import Column, Boolean, String
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import re
+from fastapi import HTTPException
+from pydantic import BaseModel, EmailStr, validator
+
+import settings
+
+# Подключение к БД
+
+engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
+
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+##################
+
+
 
 app = FastAPI()
-
-
-@app.get('/')
-def home():
-    return {'key': 'String'}
