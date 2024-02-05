1ca5a26b2039ca0233c370f59e2b3f51ded00d4c
mialkin <mialkin@bolid.ru>
2024-02-05 17:28:44 +0300

    feat: changed main.py to run app.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 from api.handlers import user_router
 
 #  Создание экземпляра приложения
+
 app = FastAPI(title="Мой проект")
 
 main_api_router = APIRouter()
 
 main_api_router.include_router(user_router, prefix='/user', tags=['user'])
 app.include_router(main_api_router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.2', port=8000)
