3834b7d3160b37c5ce795208e1b8652817936c55
mialkin <mialkin@bolid.ru>
2024-02-05 10:00:08 +0300

    feat: creating a UserDAL class to interact with User.

--- main.py
+++ main.py
@@ -35,5 +35,17 @@ class User(Base):
     is_active = Column(Boolean, default=True)
 
 
+# Класс для взаимодействия с Users
+
+class UserDAL:
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(self, name: str, surname: str, email: str) -> User:
+        new_user = User(name=name, surname=surname, email=email)
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
+
 
 app = FastAPI()
