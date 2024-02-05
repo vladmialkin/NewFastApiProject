f325d6c39cb90386a1b5f51c1e9ed58f535b7b8e
mialkin <mialkin@bolid.ru>
2024-02-05 17:28:02 +0300

    feat: creating get, post, patch and delete methods for the user (in patch there is a 422 error where only one field is saved).

--- api/models.py
+++ api/models.py
@@ -1,8 +1,9 @@
 import re
 import uuid
+from typing import Optional
 
 from fastapi import HTTPException
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, constr, validator
 from pydantic.functional_validators import field_validator
 
 # API модели Pydantic
@@ -12,7 +13,7 @@ LETTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
 
 class TunedModel(BaseModel):
     class Config:
-        orm_mode = True
+        from_attributes = True
 
 
 class ShowUser(TunedModel):
@@ -33,4 +34,34 @@ class UserCreate(BaseModel):
     def validate_name(cls, value: str) -> str:
         if not LETTER_MATCH_PATTERN.match(value):
             raise HTTPException(status_code=422, detail="Поле должно содержать только буквы")
+        return value
+
+
+class DeleteUserResponse(BaseModel):
+    deleted_user_id: uuid.UUID
+
+
+class GetUserResponse(BaseModel):
+    get_user_id: uuid.UUID
+
+
+class UpdatedUserResponse(BaseModel):
+    updated_user_id: uuid.UUID
+
+
+class UpdateUserRequest(BaseModel):
+    name: Optional[constr(min_length=1)]
+    surname: Optional[constr(min_length=1)]
+    email: Optional[EmailStr]
+
+    @validator("name", pre=True)
+    def validate_name(cls, value):
+        if value is not None and not LETTER_MATCH_PATTERN.match(value):
+            raise ValueError("Name should contain only letters")
+        return value
+
+    @validator("surname", pre=True)
+    def validate_surname(cls, value):
+        if value is not None and not LETTER_MATCH_PATTERN.match(value):
+            raise ValueError("Surname should contain only letters")
         return value
