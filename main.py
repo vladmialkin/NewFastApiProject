3064b4cdd531b51200cf72d670ee44633dc1cbb4
mialkin <mialkin@bolid.ru>
2024-02-05 09:56:26 +0300

    feat: creating the Users database table.

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 
 ##################
 
+# Таблицы БД
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4())
+    name = Column(String, nullable=False)
+    surname = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    is_active = Column(Boolean, default=True)
+
 
 
 app = FastAPI()
